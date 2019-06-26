f303e7c621209bbe789f7e815d9a0029071c4960
koshtaayush <koshtaayush@gmail.com>
2019-06-27 01:58:32 +0530

    2 files modified

--- Input.jsx
+++ Input.jsx
@@ -8,7 +8,9 @@ export default class Input extends Component {
     super(props);
     this.state = ({
       venues : [],
-      participants: []
+      participants: [],
+      selectionList:[],
+      selectionCount: []
     })
   }
 
@@ -18,6 +20,10 @@ export default class Input extends Component {
       participants: []
     });
     let ne = this.refs.venueSearchText.value;
+    if(ne == ""){
+      return false;
+    }
+
     const url = "https://api.foursquare.com/v2/venues/search?client_id=GOYOWCND2OYMVHWLQSXIGNCFWJMRNQPP3N1HZ3J1M1UIZXHT&client_secret=DZBJ0LH5CIP0Z2GG0TNJHY153YT34EF0XFJH0ZD0HEEJ4WT2&query=lunch&near={ne}&v=20170801&limit=3";
 
     url = url.replace("{ne}", ne);
@@ -29,35 +35,92 @@ export default class Input extends Component {
     });
   }
 
+  // changeSelection(idp, col){
+  //   var l = this.state.participants.slice();
+  //   console.log("col clicked" +JSON.stringify(l[idp-1]));
+  //   var t = l[idp-1].props.children[col].props.children == "NS" ? "S" : "NS";
+  //   l[idp-1] = 
+  //    <tr>
+  //       <th><input /></th>
+  //       <th id={idp + 0} onClick={() => this.changeSelection(idp, 0)}>{t}</th>
+  //       <th id={idp + 1} onClick={() => this.changeSelection(idp, 1)}>{t}</th>
+  //       <th id={idp + 2} onClick={() => this.changeSelection(idp, 2)}>{t}</th>
+  //   </tr> ;
+  //   this.setState({
+  //     participants : l
+  //   })
+  // }
+
   changeSelection(idp, col){
-    var l = this.state.participants.slice();
-    console.log("col clicked" +JSON.stringify(l[idp-1]));
-    var t = l[idp-1].props.children[col].props.children == "NS" ? "S" : "NS";
-    l[idp-1] = 
+    console.log("in th");
+    var sl = this.state.selectionList.slice();
+    console.log("idp" +idp);
+    console.log("col" +col);
+    console.log("sdfs "+this.state.selectionList[this.state.selectionList.length-1][0]);
+
+    if(col == 0){
+      sl[idp] = [1,0,0];
+    }else if(col == 1){
+      sl[idp] = [0,1,0];
+    }else if(col == 2){
+      sl[idp] = [0,0,1];
+    }
+
+    this.setState({
+      selectionList: sl
+    }, ()=>{
+      console.log(this.state.selectionList);
+      console.log("sdfs "+this.state.selectionList[this.state.selectionList.length-1][0]);
+
+      var l = this.state.participants.slice();
+      var ide = this.state.selectionList.length;
+    
+    l[idp] = 
      <tr>
-        <th><input /></th>
-        <th id={idp + 0} onClick={() => this.changeSelection(idp, 0)}>{t}</th>
-        <th id={idp + 1} onClick={() => this.changeSelection(idp, 1)}>{t}</th>
-        <th id={idp + 2} onClick={() => this.changeSelection(idp, 2)}>{t}</th>
-    </tr> ;
+          <th><input /></th>
+          <th id={ide + 0} onClick={() => this.changeSelection(ide-1, 0)}>
+          {this.state.selectionList[ide-1][0] == 1 ? "S" : "NS"}
+          </th>
+          <th id={ide + 1} onClick={() => this.changeSelection(ide-1, 1)}>
+          {this.state.selectionList[ide-1][1] == 1 ? "S" : "NS"}
+          </th>
+          <th id={ide + 2} onClick={() => this.changeSelection(ide-1, 2)}>
+          {this.state.selectionList[ide-1][2] == 1 ? "S" : "NS"}
+          </th>
+        </tr> ;
     this.setState({
       participants : l
+    })
     })
   }
 
   addParticipant(){
     var plist = this.state.participants.slice();
-    var ide = this.state.participants.length + 1;
-    plist.push(
-      <tr>
-        <th><input /></th>
-        <th id={ide + 0} onClick={() => this.changeSelection(ide, 0)}>NS</th>
-        <th id={ide + 1} onClick={() => this.changeSelection(ide, 1)}>NS</th>
-        <th id={ide + 2} onClick={() => this.changeSelection(ide, 2)}>NS</th>
-      </tr>
-    );
+    var slist = this.state.selectionList.slice();
+    slist.push([0,0,0]);
     this.setState({
-      participants : plist
+      selectionList : slist
+    }, () => {
+      var ide = this.state.selectionList.length;
+      // console.log(ide);
+      // console.log(this.state.selectionList);
+      plist.push(
+        <tr>
+          <th><input /></th>
+          <th id={ide + 0} onClick={() => this.changeSelection(ide-1, 0)}>
+          {this.state.selectionList[ide-1][0] == 1 ? "S" : "NS"}
+          </th>
+          <th id={ide + 1} onClick={() => this.changeSelection(ide-1, 1)}>
+          {this.state.selectionList[ide-1][1] == 1 ? "S" : "NS"}
+          </th>
+          <th id={ide + 2} onClick={() => this.changeSelection(ide-1, 2)}>
+          {this.state.selectionList[ide-1][2] == 1 ? "S" : "NS"}
+          </th>
+        </tr>
+      );
+      this.setState({
+        participants : plist
+      })
     })
   }
 
