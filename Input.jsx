6ad709298507dc5b57d69a44564bd115892caa58
koshtaayush <koshtaayush@gmail.com>
2019-06-26 23:24:08 +0530

    1 file modified

--- Input.jsx
+++ Input.jsx
@@ -13,20 +13,34 @@ export default class Input extends Component {
   }
 
   getSearchResult(){
-    console.log("inside fucntiuo");
     const url = "https://api.foursquare.com/v2/venues/search?client_id=GOYOWCND2OYMVHWLQSXIGNCFWJMRNQPP3N1HZ3J1M1UIZXHT&client_secret=DZBJ0LH5CIP0Z2GG0TNJHY153YT34EF0XFJH0ZD0HEEJ4WT2&query=lunch&near=Amsterdam&v=20170801&limit=3";
 
     axios.get(url).then(resp => {
-      var venueRow = 
-      console.log(resp.data.response.venues);
       this.setState({
         venues : resp.data.response.venues
       })
     });
   }
 
-  addParticipant(){
+  changeSelection(id, col){
+    var l = this.state.participants.slice();
+    console.log("col clicked" +JSON.stringify(l[id-1]));
+  }
 
+  addParticipant(){
+    var plist = this.state.participants.slice();
+    var ide = this.state.participants.length + 1;
+    plist.push(
+      <tr>
+        <th><input /></th>
+        <th id={ide + 0} onClick={() => this.changeSelection(ide, 0)}>NS</th>
+        <th id={ide + 1} onClick={() => this.changeSelection(ide, 1)}>NS</th>
+        <th id={ide + 2} onClick={() => this.changeSelection(ide, 2)}>NS</th>
+      </tr>
+    );
+    this.setState({
+      participants : plist
+    })
   }
 
   render() {
@@ -47,7 +61,7 @@ export default class Input extends Component {
               {this.state.venues[1] ? <th>{this.state.venues[1].name}</th> : null}
               {this.state.venues[2] ? <th>{this.state.venues[2].name}</th> : null}
             </tr>
-            {participantList}
+            {this.state.participants}
           </tbody>
         </table> 
         <button onClick={() => this.addParticipant()}>Add participant</button>
